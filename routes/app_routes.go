8bdb3baae8797970006eec13a54b6384fc444461
rafidkotta <getrafid@gmail.com>
2022-01-29 21:56:17 +0400

    group option added

--- routes/app_routes.go
+++ routes/app_routes.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-type client struct{
+type client struct {
 	connection *websocket.Conn
 	username   string
 	userID     string
@@ -21,16 +21,36 @@ type Message struct {
 	To      string `json:"to"`
 }
 
+type SocketEvent struct {
+	Event   string      `json:"event"`
+	Payload interface{} `json:"payload"`
+}
+
+type Group struct {
+	Name         string   `json:"name"`
+	Id           string   `json:"id"`
+	Participants []string `json:"participants"`
+}
+
+type joinGroupRequest struct {
+	GroupId string `json:"name"`
+	Client  client `json:"client"`
+}
+
 var clients = make(map[client]client) // Note: although large maps with pointer-like types (e.g. strings) as keys are slow, using pointers themselves as keys is acceptable and fast
+var groups = make(map[string]Group)
 var register = make(chan client)
 var broadcast = make(chan Message)
 var unregister = make(chan client)
+var createGroup = make(chan Group)
+var joinGroup = make(chan joinGroupRequest)
+var leaveGroup = make(chan joinGroupRequest)
 
 func ChatRoutes(app *fiber.App) {
 	app.Use(CheckUser)
 	go runHub()
 	app.Get("/chat/:username", websocket.New(func(c *websocket.Conn) {
-		client := getClient(c)
+		client := createClient(c)
 		defer func() {
 			unregister <- client
 			c.Close()
@@ -42,15 +62,6 @@ func ChatRoutes(app *fiber.App) {
 
 		for {
 			messageType, message, err := c.ReadMessage()
-			log.Println("msg : ",string(message), err)
-			data := Message{}
-			err = json.Unmarshal(message,&data)
-			if err != nil {
-				log.Println("json parse error:", err)
-			}
-			log.Println("json :",data.Message)
-			log.Println("*************************************")
-			data.From = client.username
 			if err != nil {
 				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 					log.Println("read error:", err)
@@ -58,10 +69,15 @@ func ChatRoutes(app *fiber.App) {
 
 				return // Calls the deferred function, i.e. closes the connection on error
 			}
-
+			log.Println("msg : ", string(message), err)
 			if messageType == websocket.TextMessage {
+				socketEvent := SocketEvent{}
+				err = json.Unmarshal(message, &socketEvent)
+				if err != nil {
+					log.Println("json error : #message , error:", err)
+				}
 				// Broadcast the received Message
-				broadcast <- data
+				handleSocketEvents(client, socketEvent)
 			} else {
 				log.Println("websocket Message received of type", messageType)
 			}
@@ -69,8 +85,62 @@ func ChatRoutes(app *fiber.App) {
 	}))
 }
 
-func CheckUser (c *fiber.Ctx) error {
-	path := strings.Split(c.Path(),"chat/")
+func handleSocketEvents(client client, socketEvent SocketEvent) {
+	switch socketEvent.Event {
+	case "message":
+		message := Message{}
+		b, err := json.Marshal(socketEvent.Payload)
+		if err != nil {
+			log.Println("json encode error:", err)
+		}
+		err = json.Unmarshal(b, &message)
+		if err != nil {
+			log.Println("json decode error:", err)
+		}
+		message.From = client.username
+		broadcast <- message
+	case "create_group":
+		group := Group{}
+		b, err := json.Marshal(socketEvent.Payload)
+		if err != nil {
+			log.Println("json encode error:", err)
+		}
+		err = json.Unmarshal(b, &group)
+		if err != nil {
+			log.Println("json decode error:", err)
+		}
+		group.Id = uuid.New().String()
+		group.Participants = append(group.Participants, client.username)
+		createGroup <- group
+	case "join_group":
+		join := joinGroupRequest{}
+		b, err := json.Marshal(socketEvent.Payload)
+		if err != nil {
+			log.Println("json encode error:", err)
+		}
+		err = json.Unmarshal(b, &join)
+		if err != nil {
+			log.Println("json decode error:", err)
+		}
+		join.Client = client
+		joinGroup <- join
+	case "leave_group":
+		join := joinGroupRequest{}
+		b, err := json.Marshal(socketEvent.Payload)
+		if err != nil {
+			log.Println("json encode error:", err)
+		}
+		err = json.Unmarshal(b, &join)
+		if err != nil {
+			log.Println("json decode error:", err)
+		}
+		join.Client = client
+		leaveGroup <- join
+	}
+}
+
+func CheckUser(c *fiber.Ctx) error {
+	path := strings.Split(c.Path(), "chat/")
 	if len(path) < 2 {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": true,
@@ -84,7 +154,7 @@ func CheckUser (c *fiber.Ctx) error {
 			"msg":   "Username not provided",
 		})
 	}
-	if !checkUsername(username){
+	if !checkUsername(username) {
 		return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
 			"error": true,
 			"msg":   "Username already taken",
@@ -93,7 +163,7 @@ func CheckUser (c *fiber.Ctx) error {
 	return c.Next()
 }
 
-func getClient (c *websocket.Conn) client {
+func createClient(c *websocket.Conn) client {
 	username := c.Params("username")
 	return client{
 		connection: c,
@@ -110,43 +180,206 @@ func runHub() {
 			log.Println("connection registered")
 
 		case message := <-broadcast:
-			//log.Println("Message received:", message)
+			log.Println("Message received:", message)
 
+			_, receivers := getReceivers(message.To)
 			// Send the Message To all clients
-			for connection := range clients {
-				if connection.username != message.From {
-					if message.To != "all" && connection.username == message.To {
-						if err := connection.connection.WriteMessage(websocket.TextMessage, []byte(message.Message)); err != nil {
-							log.Println("write error:", err)
-							connection.connection.WriteMessage(websocket.CloseMessage, []byte{})
-							connection.connection.Close()
-							delete(clients, connection)
-						}
-					}  else if message.To == "all" {
-						if err := connection.connection.WriteMessage(websocket.TextMessage, []byte(message.Message)); err != nil {
-							log.Println("write error:", err)
-							connection.connection.WriteMessage(websocket.CloseMessage, []byte{})
-							connection.connection.Close()
-							delete(clients, connection)
-						}
-					}
-				}
-			}
+			sendMessage(receivers, message)
 
 		case connection := <-unregister:
 			// Remove the client from the hub
 			delete(clients, connection)
 
 			log.Println("connection unregistered")
+
+		case group := <-createGroup:
+			// Create group
+			oldGroup := getGroup(group.Name)
+			if oldGroup != nil {
+				message := Message{
+					Message: "group already exists",
+					From:    "server",
+					To:      "user/" + group.Participants[0],
+				}
+				_, receivers := getReceivers(message.To)
+				sendMessage(receivers, message)
+				log.Println("group already exists", groups)
+				return
+			}
+			groups[group.Name] = group
+			message := Message{
+				Message: "Group created",
+				From:    "server",
+				To:      "user/" + group.Participants[0],
+			}
+			_, receivers := getReceivers(message.To)
+			sendMessage(receivers, message)
+			log.Println("group created", groups)
+
+		case join := <-joinGroup:
+			// Join group
+			message := joinAGroup(join)
+			_, receivers := getReceivers(message.To)
+			sendMessage(receivers, message)
+
+		case leave := <-leaveGroup:
+			// Leave group
+			message := leaveAGroup(leave)
+			_, receivers := getReceivers(message.To)
+			sendMessage(receivers, message)
 		}
 	}
 }
 
 func checkUsername(username string) bool {
-	for user := range clients{
+	for user := range clients {
 		if user.username == username {
 			return false
 		}
 	}
 	return true
+}
+
+func getGroup(name string) *Group {
+	for _, grp := range groups {
+		if grp.Name == name {
+			return &grp
+		}
+	}
+	return nil
+}
+
+func checkIsParticipant(group Group, client client) bool {
+	for _, part := range group.Participants {
+		if client.username == part {
+			return true
+		}
+	}
+	return false
+}
+
+func getGroupClients(to Group) []client {
+	var groupClients []client
+	participants := to.Participants
+	for _, part := range participants {
+		client := getClient(part)
+		if client != nil {
+			groupClients = append(groupClients, *client)
+		}
+	}
+	return groupClients
+}
+
+func getClient(username string) *client {
+	for client := range clients {
+		if client.username == username {
+			return &client
+		}
+	}
+	return nil
+}
+
+func leaveAGroup(join joinGroupRequest) Message {
+	group := getGroup(join.GroupId)
+	if group == nil {
+		return Message{
+			Message: "No group found to leave",
+			From:    "server",
+			To:      "user/" + join.Client.username,
+		}
+	}
+	if !checkIsParticipant(*group, join.Client) {
+		return Message{
+			Message: "Not a member of group",
+			From:    "server",
+			To:      "user/" + join.Client.username,
+		}
+	}
+	group.Participants = RemoveParticipant(group.Participants, join.Client.username)
+	groups[join.GroupId] = *group
+	return Message{
+		Message: join.Client.username + " left group",
+		From:    join.Client.username,
+		To:      "/group/" + group.Name,
+	}
+}
+
+func findPosition(users []string, user string) int {
+	for i, str := range users {
+		if str == user {
+			return i
+		}
+	}
+	return -1
+}
+
+func RemoveParticipant(participants []string, user string) []string {
+	index := findPosition(participants, user)
+	if index >= 0 {
+		ret := make([]string, 0)
+		ret = append(ret, participants[:index]...)
+		return append(ret, participants[index+1:]...)
+	} else {
+		return participants
+	}
+}
+
+func joinAGroup(join joinGroupRequest) Message {
+	group := getGroup(join.GroupId)
+	if group == nil {
+		return Message{
+			Message: "No group found to join",
+			From:    "server",
+			To:      "user/" + join.Client.username,
+		}
+	}
+	if checkIsParticipant(*group, join.Client) {
+		return Message{
+			Message: "Already a member of group",
+			From:    "server",
+			To:      "user/" + join.Client.username,
+		}
+	}
+	group.Participants = append(group.Participants, join.Client.username)
+	groups[group.Name] = *group
+	return Message{
+		Message: join.Client.username + " joined this group",
+		From:    join.Client.username,
+		To:      "/group/" + group.Name,
+	}
+}
+
+func sendMessage(receivers []client, message Message) {
+	for _, receiver := range receivers {
+		if receiver.username != message.From {
+			if err := receiver.connection.WriteMessage(websocket.TextMessage, []byte(message.Message)); err != nil {
+				log.Println("write error:", err)
+				receiver.connection.WriteMessage(websocket.CloseMessage, []byte{})
+				receiver.connection.Close()
+				delete(clients, receiver)
+			}
+		}
+	}
+}
+
+func getReceivers(to string) (string, []client) {
+	var Clients []client
+	if strings.Contains(to, "group/") {
+		groupId := strings.Split(to, "group/")[1]
+		group := getGroup(groupId)
+		if group == nil {
+			return "Group", Clients
+		}
+		Clients = append(Clients, getGroupClients(*group)...)
+		return "Group", Clients
+	} else if strings.Contains(to, "user/") {
+		username := strings.Split(to, "user/")[1]
+		c := getClient(username)
+		if c == nil {
+			return "user", Clients
+		}
+		Clients = append(Clients, *c)
+		return "user", Clients
+	}
+	return "default", Clients
 }
